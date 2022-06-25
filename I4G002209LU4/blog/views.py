8a6ef00b1241c355d709c5ed3262b8d12890834c
Neon-jeff <omaksteejay@gmail.com>
2022-06-25 21:44:30 +0100

    ADDED TEMPLATES

--- I4G002209LU4/blog/views.py
+++ I4G002209LU4/blog/views.py
@@ -1,3 +1,23 @@
+from pyexpat import model
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
+
+
+
+from I4G002209LU4.blog.models import Post
+
+class PostListView(ListView):
+    model=Post
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+class PostUpdateView(DetailView):
+    model = Post
+class PostDeleteView()
